c96cd44cd84e29955a9bdd102085ecfe9af1bc1c
PC <PC@DESKTOP-8OL11B4>
2023-12-19 18:32:57 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+using TravelTripProje.Controllers;
+
+namespace TravelTripProje.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+
+        public ActionResult Index()
+        {
+            var degeler=c.Blogs.ToList();
+            return View(degeler);
+        }
+        [HttpGet]
+
+        public ActionResult YeniBlog()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p )
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil (int id)
+        {
+            var b =c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir (int id)
+        {
+            var bl=c.Blogs.Find(id);
+            return View("BlogGetir",bl);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.Id);
+            blg.Aciklama= b.Aciklama;
+            blg.Baslik=b.Baslik;
+            blg.BlogImage=b.BlogImage;
+            blg.Tarih=b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult YorumGetir (int id)
+        {
+            var yr=c.Yorumlars.Find(id);
+            return View("YorumGetir", yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.Id);
+            yrm.KullaniciAdi= y.KullaniciAdi;  
+            yrm.Yorum=y.Yorum;
+            yrm.Mail=y.Mail;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProje/Models/Sınıflar/AdresBlog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class AdresBlog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public string  AdresAcik {  get; set; }
+        public int Telefon { get; set; }
+        public string Konum { get; set; }
+
+
+
+    }
+}
